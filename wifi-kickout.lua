9cb434d76ab07ffab4743d1c978bb8dcc3b013ae
nivrrex <nivrrex@gmail.com>
2020-10-13 16:56:42 +0800

    增加命令行参数，可设置是否一直循环，可设置仅监测2.4G或5G网络

--- wifi-kickout.lua
+++ wifi-kickout.lua
@@ -1,5 +1,6 @@
 #!/usr/bin/lua
 require "ubus"
+local sleep = require('sleep')
 
 function log_sys(msg,logfile)
     local reset = string.format("%s[%sm",string.char(27), tostring(0))
@@ -49,6 +50,21 @@ function get_mac_list(wlan)
     end
     return data
 end
+function get_mac_list2(wlan)
+    data = {}
+    local status = conn:call("hostapd." .. wlan,"get_clients",{})
+    if status ~= nil then
+        for index,value in pairs(status) do
+            if index == "clients" then
+                for mac,client in pairs(value) do
+                    table.insert(data,mac)
+                    print("mac",mac)
+                end
+            end
+        end
+    end
+    return data
+end
 function get_signal(wlan, mac_client)
     local signal = 0
     local status = conn:call("iwinfo","assoclist",{device=wlan,mac=mac_client})
@@ -84,14 +100,26 @@ function table.match(tbl,value)
     end
     return false,nil
 end
+function arg_check(arg,check)
+    if arg == nil then 
+        return false 
+    else
+        if string.upper(arg) == string.upper(check) then
+            return true
+        end
+    end
+end
 
 local black_list = {"00:00:00:00:00:00"}
 local white_list = {"00:00:00:00:00:00"}
-local only24g_list = {"00:00:00:00:00:00"}
+local only24g_list = {"00:00:00:00:00:00"} --对应仅支持2.4G的终端
+
+local only_5g = false   --对应路由器
+local only_24g = false  --对应路由器
 
 local kickout_24G = -85
-local kickout_5G = -75
-local kickout_24G_5G = -55
+local kickout_5G = -76
+local kickout_24G_5G = -60
 
 logtofile = "/var/log/wifi-kickout.log"
 logfile = io.open(logtofile, "a")
@@ -106,7 +134,20 @@ for index,value in pairs(get_wlan_list()) do
     wlan[value]["MHz"] = check_mhz(value)
 end
 
-while true do
+--命令行参数，设置是否一直循环
+if arg_check(arg[1],"ALWAYS") then always = true else always = false end
+--命令行参数，设置是否仅仅监测2.4G或者仅仅监测5G
+if arg_check(arg[2],"ONLY_24G") then
+    only_24g = true
+    only_5g = false
+elseif arg_check(arg[2],"ONLY_5G") then
+    only_5g = true
+    only_24g = false
+else
+    local only_5g = false
+    local only_24g = false
+end
+repeat
     local mac = {}
     for _,value in pairs(wlan) do
         local value_mac_array = get_mac_list(value["Name"])
@@ -137,13 +178,15 @@ while true do
             if table.match(black_list, mac_index ) then
                 kick_out(wlan_name, mac_index, 3000)
                 log_sys(string.format("Kickout the client %s , because in black list", mac_index), logfile)
-            elseif signal_24g < kickout_24G then
+            elseif (only_24g == true) and (only_5g == true) then
+                log_sys(string.format("logical error"), logfile)
+            elseif (only_5g == false) and (signal_24g < kickout_24G) then
                 log_sys(string.format("Kickout the client %s , %d > %d (2.4G)", mac_index, signal_24g, kickout_24G), logfile)
                 kick_out(wlan_name, mac_index, 3000)
-            elseif signal_5g < kickout_5G then
+            elseif (only_24g == false) and (signal_5g < kickout_5G) then
                 log_sys(string.format("Kickout the client %s , %d > %d (5G)", mac_index, signal_5g, kickout_5G), logfile)
                 kick_out(wlan_name, mac_index, 3000)
-            elseif (signal_24g ~= 0) and (signal_5g == 0) and (signal_24g > kickout_24G_5G) then
+            elseif (only_5g == false and only_24g == false) and (signal_24g ~= 0) and (signal_5g == 0) and (signal_24g > kickout_24G_5G) then
                 if not table.match(only24g_list, mac_index ) then
                     log_sys(string.format("Kickout the client %s , %d < %d (change 2.4G to 5G)", mac_index, signal_24g, kickout_24G_5G), logfile)
                     kick_out(wlan_name, mac_index, 3000)
@@ -151,7 +194,10 @@ while true do
             end
         end
     end
-    os.execute("sleep 30")
-end
+    if always then
+      --os.execute("sleep 30")
+        sleep(30 * 1000)
+    end
+until not always 
 logfile:close()
 ubus.stop(conn)
